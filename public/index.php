98225fa5779214400f830efac5e2688a6ccacb42
Khaled Benharrat <benk.devweb@gmail.com>
2020-04-13 19:41:32 +0200

    Quest finish

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use App\Wcs\Hello;
+
+$hello = new Hello();
+echo '<h1>' . $hello->talk() . '</h1>';
+echo '<h2>' . $hello->comment() . '</h2><br>';
+
+use HelloWorld\SayHello;
+$helloMan = new SayHello();
+echo '<h1>' . $helloMan->world() . '</h1>';
+echo '<h2>' . $hello->comment2() . '</h2><br>';
+
+
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Wcs;
+
+class Hello
+{
+
+    public function talk() {
+        return 'Hello world !';
+    }
+
+    public function comment() {
+        return 'J\'ai un peu galéré sur cette quête !';
+    }
+
+    public function comment2() {
+        return 'J\'ai un peu moins galéré sur cette quête !';
+    }
+
+
+}
